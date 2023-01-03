1b8e2135c13adbf134ebf2c064771019752bf33d
iSo <ilja.hh@gmail.com>
2023-01-03 12:58:07 +0100

    initial commit 2

---
+++ terraform.tf
@@ -0,0 +1,21 @@
+terraform {
+/*
+  cloud {
+    organization = "pf-berlin"
+
+    workspaces {
+      name = "learn-terraform-cloud"
+    }
+  }
+ */
+
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16.0"
+    }
+  }
+
+  required_version = ">= 0.14.0"
+}
